d07edffed3bd04235ba335913e7873a25022f7c0
UotanKlein <144787608+UotanKlein@users.noreply.github.com>
2023-10-13 15:38:20 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -2,6 +2,6 @@ const fuelConsumption = 10; //Расход топлива
 const distance = 100; //Дистанция
 const priceFuel = 1000; //Стоимость
 
-const result = (distance / fuelConsumption) * priceFuel;
+const result = distance * fuelConsumption * priceFuel / 100;
 
 console.log(result)
